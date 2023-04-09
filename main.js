ad832dc5a91afb9160d9026482ba08218e3097a7
Manu1310 <113275871+Manu1310@users.noreply.github.com>
2023-04-09 12:36:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+prediction_1 = ""
+prediction_2 = ""
+
+Webcam.set({
+    width:350,
+    heigth:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot() { 
+    Webcam.snap(function(data_uri) { 
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>'; 
+    }); 
+}
+
+console.log('ml5 version - ',ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/1r-9MNUFE/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log('modelLoaded');
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is" + prediction_1;
+    speak_data_2 = "The second prediction is" + prediction_2;
+    var utterthis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterthis);
+}
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img,gotresult);
+    }
+
+function gotresult(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML = results[0].label;
+        document.getElementById("result_gesture_name2").innerHTML = results[1].label;
+    
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+    
+        speak();
+        if(results[0].label == "Amazing"){
+            document.getElementById("update_hand_gesture").innerHTML = "&#128076;"
+        }
+        if(results[0].label == "Best"){
+            document.getElementById("update_hand_gesture").innerHTML = "&#128077;"
+        } 
+        if(results[0].label == "Victory"){
+            document.getElementById("update_hand_gesture").innerHTML = "&#9996;"
+        }
+    
+        if(results[1].label == "Amazing"){
+            document.getElementById("update_hand_gesture2").innerHTML = "&#128076;"
+        }
+        if(results[1].label == "Best"){
+            document.getElementById("update_hand_gesture2").innerHTML = "&#128077;"
+        }
+        if(results[1].label == "Victory"){
+            document.getElementById("update_hand_gesture2").innerHTML = "&#9996;"
+        }
+    }
+    }
